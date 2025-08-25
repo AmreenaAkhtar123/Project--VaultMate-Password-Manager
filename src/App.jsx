d0cbbad1ed342eac3689302f83efd3c46843fdb7
AmreenaAkhtar123 <amberkhannn7654@gmail.com>
2025-08-25 16:40:40 +0500

    Initializing and Setting my project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+	return (
+		<>
+			<div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+			<div className='text-5xl'>
+				i am a home page
+
+			</div>
+		</>
+	)
+}
+
+export default App
